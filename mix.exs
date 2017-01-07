932bd1dac1608f14b2d2f4f531c6f8eb419101c9
Jim Menard <jim.menard@chloeandisabel.com>
2017-01-07 08:51:02 -0500

    specify Elixir 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Happi.Mixfile do
   def project do
     [app: :happi,
      version: "0.0.1",
-     elixir: "~> 1.5-dev",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
